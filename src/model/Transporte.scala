b365ee32058e7edec344beb145d55abab3630ff5
Juan Pablo Jacob <jacobjuampe@hotmail.com>
2014-10-31 12:51:09 -0300

    Cambio nombre variable

--- src/model/Transporte.scala
+++ src/model/Transporte.scala
@@ -2,13 +2,13 @@ package model
 
 trait Transporte {
 
-  var tipo_transporte: TipoTransporte = null
-  var seguimiento: Seguimiento = null
   var capacidad: Int = 0
   var costo: Int = 0
   var velocidad: Int = 0
   var envios: List[Envio] = List()
   var destino: Sucursal = null
+  var seguimiento: Seguimiento = null
+  var tipo: TipoTransporte = null
   
   def capacidadDisponible = capacidad - envios.map(e => e.volumen).sum
   
