7c242c4a45f9a53c79076d68a36d2a671e4176e8
Agustin Pina <aguspina87@gmail.com>
2014-10-31 12:25:17 -0300

    change volumen in Sucursal

--- src/model/Envio.scala
+++ src/model/Envio.scala
@@ -2,7 +2,10 @@ package model
 
 class Envio(var paquete: Paquete = null, var destino: Sucursal = null) {
   
-  def asignarTransporte(transporte: Transporte) = transporte.agregarEnvio(this)
+  def asignarTransporte(transporte: Transporte) = {
+    transporte.agregarEnvio(this)
+    destino.agregarEnvioEntrante(this)
+  }
   
   def esRefrigerdo = false
   
--- src/model/Sucursal.scala
+++ src/model/Sucursal.scala
@@ -2,8 +2,17 @@ package model
 
 class Sucursal(val capacidad: Int) {
 
-	var enviosSalientes: List[Envio] = List()
-    var transportes: List[Transporte] = List()
-    
-    def capacidadDisponible = capacidad - enviosSalientes.map(e => e.volumen).sum
+  var enviosSalientes: List[Envio] = List()
+  var enviosEntrantes: List[Envio] = List()
+  var transportes: List[Transporte] = List()
+
+  def capacidadDisponible = capacidad - volumenEnvios
+  
+  def volumenEnvios = enviosSalientes.map(e => e.volumen).sum + enviosEntrantes.map(e => e.volumen).sum 
+
+  def agregarEnvioEntrante(envio: Envio) = {
+    if (capacidadDisponible > envio.volumen) {
+      enviosEntrantes = envio :: enviosEntrantes
+    }
+  }
 }
