46c4134a113d86046535b6e9072a9a286436fea6
Agustin Pina <aguspina87@gmail.com>
2014-10-31 13:02:37 -0300

    rename enviosAlmacenados

--- src/model/Sucursal.scala
+++ src/model/Sucursal.scala
@@ -2,13 +2,13 @@ package model
 
 class Sucursal(val capacidad: Int) {
 
-  var enviosSalientes: List[Envio] = List()
+  var enviosAlmacenados: List[Envio] = List()
   var enviosEntrantes: List[Envio] = List()
   var transportes: List[Transporte] = List()
 
   def capacidadDisponible = capacidad - volumenEnvios
   
-  def volumenEnvios = enviosSalientes.map(e => e.volumen).sum + enviosEntrantes.map(e => e.volumen).sum 
+  def volumenEnvios = enviosAlmacenados.map(e => e.volumen).sum + enviosEntrantes.map(e => e.volumen).sum 
 
   def agregarEnvioEntrante(envio: Envio) = {
     if (capacidadDisponible > envio.volumen) {
